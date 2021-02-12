ecf1bab41363a23690d83c5df998508ae2edd861
abaur <abaur@student.42.fr>
2021-02-12 14:21:54 +0100

    Fix unitilialized variable in philosopher struct.

--- philo_one/philosopher.c
+++ philo_one/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 14:07:46 by abaur             #+#    #+#             */
-/*   Updated: 2021/02/10 17:01:20 by abaur            ###   ########.fr       */
+/*   Updated: 2021/02/12 14:09:20 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ extern short	philo_init(int count)
 		g_hoomans[i].thread = (pthread_t){ 0 };
 		g_hoomans[i].status = phi_think;
 		g_hoomans[i].meals = 0;
+		g_hoomans[i].ttaction = 0;
 		g_hoomans[i].ttdie = g_ttdie;
 		g_hoomans[i].hands[0] = NULL;
 		g_hoomans[i].hands[1] = NULL;
