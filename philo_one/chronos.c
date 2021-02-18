8d85410f65c53b70079c0920a3ed321bcbcaf900
abaur <abaur@student.42.fr>
2021-02-18 17:47:31 +0100

    Chronos now has full agency over the flow of time.

--- philo_one/chronos.c
+++ philo_one/chronos.c
@@ -6,55 +6,64 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 14:24:47 by abaur             #+#    #+#             */
-/*   Updated: 2021/02/16 16:15:46 by abaur            ###   ########.fr       */
+/*   Updated: 2021/02/18 17:46:51 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "chronos.h"
 
+#include "minilibft/minilibft.h"
+
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
 
-#define WAITSAFEFTYBUFFER 1000
+#define FRAMEPERIOD 500
 
-static struct timeval	g_origin;
+static unsigned short	g_isrunning = 0;
+static pthread_t		g_thread = (pthread_t){ 0 };
+static __useconds_t		g_date[2] = {0, 0};
+static unsigned short	g_i = 0;
 
-/*
-** Set the current date as the origin of times.
-*/
+static void				*stopwatch_main(void *arg)
+{
+	(void)arg;
+	while (g_isrunning)
+	{
+		usleep(FRAMEPERIOD);
+		g_date[!g_i] = g_date[g_i] + FRAMEPERIOD;
+		g_i = !g_i;
+	}
+	return (NULL);
+}
 
 extern void				stopwatch_start(void)
 {
-	gettimeofday(&g_origin, NULL);
+	g_isrunning = TRUE;
+	pthread_create(&g_thread, NULL, &stopwatch_main, NULL);
+}
+
+extern void				stopwatch_stop(void)
+{
+	g_isrunning = FALSE;
+	pthread_join(g_thread, NULL);
 }
 
 /*
-** Gives the time in microseconds since the beginning.
+** Gives the time in microseconds since the origin of times.
 ** @return __useconds_t
 */
 
 extern __useconds_t		stopwatch_date(void)
 {
-	struct timeval	date;
-
-	gettimeofday(&date, NULL);
-	return (1000000 * (date.tv_sec - g_origin.tv_sec))
-	+ (date.tv_usec - g_origin.tv_usec);
+	return (g_date[g_i]);
 }
 
 extern __useconds_t		wait_until(__useconds_t target_date)
 {
-	__useconds_t current_date;
-	__useconds_t sleep_date;
+	__useconds_t	current_date;
 
-	if (0 <= WAITSAFEFTYBUFFER && WAITSAFEFTYBUFFER < target_date)
-	{
-		sleep_date = target_date - WAITSAFEFTYBUFFER;
-		current_date = stopwatch_date();
-		if (current_date < sleep_date)
-			usleep(sleep_date - current_date);
-	}
-	while ((current_date = stopwatch_date()) < target_date)
-		continue ;
-	return (current_date);
+	while((current_date = stopwatch_date()) < target_date)
+		usleep(smallest(1000, target_date - current_date));
+	return (stopwatch_date());
 }
