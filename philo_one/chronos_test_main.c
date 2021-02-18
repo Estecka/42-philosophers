ea04d0469828591abe019f19e1d8fcc4c67e08fd
abaur <abaur@student.42.fr>
2021-02-18 15:25:48 +0100

    Added max_latency to chronos tester.
    Fix duplicate thread n°0

--- philo_one/chronos_test_main.c
+++ philo_one/chronos_test_main.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 19:52:06 by abaur             #+#    #+#             */
-/*   Updated: 2021/02/16 15:36:20 by abaur            ###   ########.fr       */
+/*   Updated: 2021/02/18 15:21:29 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,17 @@ static void	*thread_main(void *uid)
 {
 	__useconds_t	target_date;
 	__useconds_t	wake_date;
+	__useconds_t	max_latency;
 
+	max_latency = 0;
+	wait_until(1000000);
 	while (1)
 	{
 		target_date = stopwatch_date() + g_sleeptime;
 		wake_date = wait_until(target_date);
-		printf("%i Latency: %+.3f ms\n", (int)uid,
-			(wake_date - target_date) / (float)1000);
+		max_latency = greatest(max_latency, wake_date - target_date);
+		printf("%2i Oversleep: %+.3f ms		Max: %.3f\n", (int)uid,
+			(wake_date - target_date) / (float)1000, max_latency / (float)1000);
 	}
 }
 
@@ -45,7 +49,8 @@ extern int	main(int argc, char **args)
 		miniatoi(args[2]) * 1000 :
 		1000000;
 	stopwatch_start();
-	while (threads--)
+	threads++;
+	while (--threads)
 		pthread_create(&trashbin, NULL, &thread_main, (void*)threads);
 	thread_main(0);
 }
