ca12d03686800ca3e96a8c8ab765fcac57b37acc
abaur <abaur@student.42.fr>
2021-02-19 16:20:22 +0100

    Fix philosophers displaying outdated timestamps.

--- philo_one/philosopher_main.c
+++ philo_one/philosopher_main.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 15:05:26 by abaur             #+#    #+#             */
-/*   Updated: 2021/02/17 16:56:13 by abaur            ###   ########.fr       */
+/*   Updated: 2021/02/19 16:17:31 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,15 @@
 
 static void	philo_sleep(t_philosopher *this)
 {
-	int				ttactionms;
 	__useconds_t	date;
 
-	ttactionms = this->ttaction / 1000;
 	date = wait_until(this->ttaction);
 	pthread_mutex_lock(&this->self);
 	if (g_sim_status == sim_playing)
 	{
 		printf("%5i %i is thinking	(+%.3f)\n",
-			ttactionms, this->uid, (date - this->ttaction) / (float)1000);
-		this->ttaction += g_ttsleep;
+			date / 1000, this->uid, (date - this->ttaction) / (float)1000);
+		this->ttaction = date + g_ttsleep;
 		this->status = phi_thinking;
 	}
 	else
@@ -62,10 +60,8 @@ static void	philo_think(t_philosopher *this)
 
 static void	philo_eat(t_philosopher *this)
 {
-	int				ttactionms;
 	__useconds_t	date;
 
-	ttactionms = this->ttaction / 1000;
 	date = wait_until(this->ttaction);
 	philo_drop_ustensiles(this);
 	pthread_mutex_lock(&this->self);
@@ -74,8 +70,8 @@ static void	philo_eat(t_philosopher *this)
 	if (g_sim_status == sim_playing)
 	{
 		printf("%5i %i is sleeping	(+%.3f)\n",
-			ttactionms, this->uid, (date - this->ttaction) / (float)1000);
-		this->ttaction += g_ttsleep;
+			date / 1000, this->uid, (date - this->ttaction) / (float)1000);
+		this->ttaction = date + g_ttsleep;
 		this->status = phi_sleeping;
 	}
 	else
